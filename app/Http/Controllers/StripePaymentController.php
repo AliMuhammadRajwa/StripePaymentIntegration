0ece111c0ae27bb3bf3575775007ca53c69a05ab
unknown <alimohdrajwa@gmail.com>
2023-08-08 20:20:04 +0500

    Tested and Worked

---
+++ app/Http/Controllers/StripePaymentController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session;
+use Stripe;
+
+class StripePaymentController extends Controller
+{
+    public function stripe()
+    {
+        return view('stripe');
+    }
+
+
+    public function stripePost(Request $request)
+    {
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+
+        Stripe\Charge::create ([
+            "amount" => 100 * 100,
+            "currency" => "usd",
+            "source" => $request->stripeToken,
+            "description" => "Ali Muhammad Payment"
+        ]);
+
+        Session::flash('success', 'Payment successful!');
+
+        return back();
+    }
+}
